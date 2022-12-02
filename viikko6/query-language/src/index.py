86e5dc81ebb801a446baf3c81fabd461deddf120
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-02 13:33:41 +0200

    vko6 teht 2

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,32 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+        HasAtLeast(5, "goals"),
+        HasAtLeast(5, "assists"),
+        PlaysIn("PHI")
+    )
+
+    matcher = All()
+
+    matcher = Not(
+        #HasAtLeast(5, "goals"),
+        #HasAtLeast(5, "assists"),
+        PlaysIn("SEA")
+    )
+    matcher = And(
+    HasFewerThan(1, "goals"),
+    PlaysIn("NYR")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
