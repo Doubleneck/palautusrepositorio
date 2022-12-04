bd4cfafcb4a4bcb18f9be9ff022d34a63646f719
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2022-12-04 18:57:01 +0200

    vko6 teht 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -2,6 +2,39 @@ from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, Or, Not, HasAtLeast, PlaysIn, All,  HasFewerThan
 
+class Build:
+    def __init__(self):
+        self.alkiot = []
+
+    def push(self, alkio):
+        self.alkiot.append(alkio)
+
+    def pop(self):
+        return self.alkiot.pop()
+
+    def empty(self):
+        return len(self.alkiot) == 0
+        
+
+class Querybuilder:
+    def __init__(self, build = Build()):
+        self.build_olio = build
+        
+        if not type(self.build_olio) is type(And()):
+            self.build_olio = All()
+    
+    def playsIn(self, team):   
+        return Querybuilder(And(self.build_olio,PlaysIn(team)))
+    
+    def hasAtLeast(self, points, attr):
+        return Querybuilder(And(self.build_olio,HasAtLeast(points,attr)))
+
+    def hasFewerThan(self, points, attr):
+        return Querybuilder(And(self.build_olio,HasFewerThan(points,attr)))
+
+    def build(self):
+        return self.build_olio
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
@@ -16,10 +49,11 @@ def main():
     matcher = All()
 
     matcher = Not(
-        #HasAtLeast(5, "goals"),
-        #HasAtLeast(5, "assists"),
+        HasAtLeast(5, "goals"),
+        HasAtLeast(5, "assists"),
         PlaysIn("SEA")
     )
+    
     matcher = And(
     HasFewerThan(1, "goals"),
     PlaysIn("NYR")
@@ -32,7 +66,12 @@ def main():
         PlaysIn("FLA"),
         PlaysIn("BOS")
     )
-)
+    )
+
+    query = Querybuilder()
+    matcher = query.build()
+    matcher = query.hasAtLeast(10, "goals").hasFewerThan(20, "goals").playsIn("NYR").build()
+
     for player in stats.matches(matcher):
         print(player)
 
